d916f1307c7cf5d6433b243b1f821537674fbd56
rimantasdotnet <rreketis@gmail.com>
2019-04-30 06:28:24 +0300

    antra pamoka (kintamieji, sumavimas,salygos,true or false)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rimak = "9";
+            Console.WriteLine("mano skaicius rimak");
+            Console.ReadLine({0} stop);
+
+        }
+    }
+}
